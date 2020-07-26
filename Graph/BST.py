5648c860ea105b50274d45b071135132f1515536
fadel <fadelsougou@gmail.com>
2020-07-26 16:36:58 +0200

    search and insert in BST + print pretty Tree

--- Graph/BST.py
+++ Graph/BST.py
@@ -7,7 +7,7 @@ class TreeNode:
 
 class BST:
     def __init__(self,inputValues):
-        self.root = TreeNode(int(inputValues[0]))
+        self.root = TreeNode(inputValues[0])
         nodeQueue = [self.root]
         front = 0
         index = 1
@@ -32,33 +32,58 @@ class BST:
                 node.right = TreeNode(rightNumber)
                 nodeQueue.append(node.right)
     
+    def prettyPrintTree(self, node=-1, prefix="", isLeft=True):
+        if node == -1: node = self.root
+        if not node:
+            print("Empty Tree")
+            return
+
+        if node.right:
+            self.prettyPrintTree(node.right, prefix + ("│   " if isLeft else "    "), False)
+
+        print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+        if node.left:
+            self.prettyPrintTree(node.left, prefix + ("    " if isLeft else "│   "), True)
+    
         
     def height(self, head = -1):
         if head == -1: head = self.root
         if head is None: return -1
         return 1 + max(self.height(head.left),self.height(head.right))
     
-    def vertices(self, head = -1):
-        if head==-1: head = self.root
-        if head is None: return 0
-        print(head.val)
-        return 1 + self.vertices(head.left) + self.vertices(head.right)
+    def order(self, head = -1):  #return the amount of vertices in self.root
+        def helper(head=self.root):
+            if head is None: return 0
+            return 1 + helper(head.left) + helper(head.right)
+        return helper()
 
-    def insert(self, val, head = -1):
-        if head == -1: head = self.root
-        if not head : return TreeNode(val)
-        elif head.val > val:
-            head.left = self.insert(val, head.left)
-            return head.left
-        else:
-            head.right = self.insert(val, head.right)
-            return head.right
+    def searchInsert(self, val):
+        def helper(head = self.root):
+            if not head : return TreeNode(val)
+            if head.val == val:
+                print(str(val)+ " is already in the BST root")
+                return head
+            elif head.val > val:
+                head.left = helper(head.left)
+            else:
+                head.right = helper(head.right)
+            return head
+        self.root = helper()
+
 
     #def delete(self, val):
 
 
-u = BST([2,0,4])
-print(u.vertices())
+zodiac = ["CAPRICORN", "AQUARIUS", "PISCES", "PISCES", "ARIES", "TAURUS", "GEMINI", "CANCER", "LEO", "VIRGO", "LIBRA", "SCORPIO"]
+u = BST([zodiac[0]])
+for val in zodiac:
+    u.searchInsert(val)
+u.prettyPrintTree()
+print(u.order())
+print(u.height())
+
+
     
 
 
